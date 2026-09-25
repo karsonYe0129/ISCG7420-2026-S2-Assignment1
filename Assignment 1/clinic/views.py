578c993c6aa44cb04a479418f60a3e2155cc7ab1
karsonYe0129 <yejunhui029@gmail.com>
2026-09-26 10:57:06 +1200

    Add patient registration login and logout

--- Assignment 1/clinic/views.py
+++ Assignment 1/clinic/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.utils import timezone
-
 from .models import Doctor, Appointment
 
 # Create your views here.
@@ -30,3 +31,18 @@ def home(request):
         'appointments': appointments,
     }
     return render(request, 'clinic/home.html', context)
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'clinic/register.html', {'form': form})
