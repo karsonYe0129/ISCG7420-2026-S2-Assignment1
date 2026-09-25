c34abe217dd7cb3ffb665c407fd56a067bd02686
karsonYe0129 <yejunhui029@gmail.com>
2026-09-25 15:03:49 +1200

    Add doctor model and display active doctors on homepage

--- Assignment 1/clinic/models.py
+++ Assignment 1/clinic/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    speciality = models.CharField(max_length=100)
+    bio = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
--- Assignment 1/clinic/views.py
+++ Assignment 1/clinic/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Doctor
 
 # Create your views here.
 def home(request):
+    doctors = Doctor.objects.filter(is_active=True).order_by('name')
+
     context = {
-        'clinic_name': 'Piki Ora Medical Center'
+        'clinic_name': 'Piki Ora Medical Center',
+        'doctors': doctors,
     }
     return render(request, 'clinic/home.html', context)
